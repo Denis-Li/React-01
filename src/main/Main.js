d83a62875d40f573a872a3c89344813714530393
Denis-Li <den.le.genplan@gmail.com>
2020-10-02 15:25:16 +0200

    start 2

---
+++ src/main/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Main.css';
+
+
+class Main extends React.Component{
+    render() {
+        return (
+            <div className="container">
+                <main>
+                    <article>
+                    <header>
+                        <h1>Article 1</h1>
+                        <h2>Article 2</h2>
+                    </header>
+                    <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quis culpa nemo expedita aperiam eum eos! At tempore sapiente autem deserunt necessitatibus, tempora fugit expedita ullam accusamus quos voluptate illo debitis eveniet ipsam quo dolores perspiciatis a excepturi in quis.
+                    </p>
+                    <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quis culpa nemo expedita aperiam eum eos! At tempore sapiente autem deserunt necessitatibus, tempora fugit expedita ullam accusamus quos voluptate illo debitis eveniet ipsam quo dolores perspiciatis a excepturi in quis.
+                    </p>
+                    </article>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Main;
