668154ab1490c441a1b08c735e2fa3c25831e790
alexkobrin <storyday2012@gmail.com>
2020-10-07 21:26:39 +0200

    Updates

---
+++ precache-manifest.4701222121b0bd59a866da96885f9813.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1c101f46a008594448ca4f82ae9f23",
+    "url": "/React-01/index.html"
+  },
+  {
+    "revision": "d49ea8f6c1d295280974",
+    "url": "/React-01/static/css/main.4904ae2c.chunk.css"
+  },
+  {
+    "revision": "9ef598736b7a1cc215cf",
+    "url": "/React-01/static/js/2.23ea4713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-01/static/js/2.23ea4713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49ea8f6c1d295280974",
+    "url": "/React-01/static/js/main.c059ec90.chunk.js"
+  },
+  {
+    "revision": "c9c78a396b8737690adb",
+    "url": "/React-01/static/js/runtime-main.74410b8b.js"
+  },
+  {
+    "revision": "1e6ae1eead98df0dadce3c7f6b10aefc",
+    "url": "/React-01/static/media/Geometria-Bold.1e6ae1ee.ttf"
+  },
+  {
+    "revision": "472a653cd97c617986d64b7071794eac",
+    "url": "/React-01/static/media/Geometria-Bold.472a653c.eot"
+  },
+  {
+    "revision": "fdd15f825a6db43442a758e2db168314",
+    "url": "/React-01/static/media/Geometria-Bold.fdd15f82.woff"
+  },
+  {
+    "revision": "c3f63f43eb57e7479229bbe5dd9dd84a",
+    "url": "/React-01/static/media/Geometria-BoldItalic.c3f63f43.woff"
+  },
+  {
+    "revision": "d10227c4f0a3118bbe35a5385d174b17",
+    "url": "/React-01/static/media/Geometria-BoldItalic.d10227c4.eot"
+  },
+  {
+    "revision": "fbab91d0e316aca18ae959600db79efe",
+    "url": "/React-01/static/media/Geometria-BoldItalic.fbab91d0.ttf"
+  },
+  {
+    "revision": "920970cd7d8eb3671c26a5eee061903e",
+    "url": "/React-01/static/media/Geometria-Italic.920970cd.ttf"
+  },
+  {
+    "revision": "a09caa037bb876d250a8ff67f7b9370d",
+    "url": "/React-01/static/media/Geometria-Italic.a09caa03.eot"
+  },
+  {
+    "revision": "e8a473fe87c37063ce67f8f8e9608073",
+    "url": "/React-01/static/media/Geometria-Italic.e8a473fe.woff"
+  },
+  {
+    "revision": "444f65439c72fdc579e727d71fa605f1",
+    "url": "/React-01/static/media/Geometria-Light.444f6543.woff"
+  },
+  {
+    "revision": "4d6a57fdab429e111b3da042aa63ef2f",
+    "url": "/React-01/static/media/Geometria-Light.4d6a57fd.eot"
+  },
+  {
+    "revision": "90ad92ba651bc1af1ce286fb2933da2d",
+    "url": "/React-01/static/media/Geometria-Light.90ad92ba.ttf"
+  },
+  {
+    "revision": "45656b73e1a07f979bfb56559b359b07",
+    "url": "/React-01/static/media/Geometria-Medium.45656b73.woff"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/React-01/static/media/Geometria-Medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "90152a4f5c76144f11d47dcf43ae12d9",
+    "url": "/React-01/static/media/Geometria-Medium.90152a4f.eot"
+  },
+  {
+    "revision": "4f5d0cff4b01ea43b37a8d35675da88b",
+    "url": "/React-01/static/media/Geometria-MediumItalic.4f5d0cff.eot"
+  },
+  {
+    "revision": "d61544a392a876884d97119256642935",
+    "url": "/React-01/static/media/Geometria-MediumItalic.d61544a3.woff"
+  },
+  {
+    "revision": "f53e6bc54776e97c6fae19447f66fffd",
+    "url": "/React-01/static/media/Geometria-MediumItalic.f53e6bc5.ttf"
+  },
+  {
+    "revision": "3c6a7d3ae76a5aec78aac69db1af15cf",
+    "url": "/React-01/static/media/Geometria.3c6a7d3a.woff"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/React-01/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "778525f6405ea0daa98d885a4ba17189",
+    "url": "/React-01/static/media/Geometria.778525f6.eot"
+  },
+  {
+    "revision": "957154e49280a63e3ce1d58ad81f7941",
+    "url": "/React-01/static/media/facebook.957154e4.svg"
+  },
+  {
+    "revision": "e75b2dfa0c261af4a1437280498f01e3",
+    "url": "/React-01/static/media/header-bg.e75b2dfa.jpg"
+  },
+  {
+    "revision": "8ce3d4832bad2dd9fef45d500539c651",
+    "url": "/React-01/static/media/instagram.8ce3d483.svg"
+  },
+  {
+    "revision": "f82c754962976d7c26369995c3d60b6c",
+    "url": "/React-01/static/media/test-icon.f82c7549.svg"
+  },
+  {
+    "revision": "0ecad0d75d0177817b77f9f4126a92f8",
+    "url": "/React-01/static/media/youtube.0ecad0d7.svg"
+  }
+]);
