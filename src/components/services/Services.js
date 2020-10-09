81567660c26276162bdf9e521cfedaaf6181f8eb
Denis-Li <den.le.genplan@gmail.com>
2020-10-10 01:31:19 +0200

    add services3

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -8,54 +8,48 @@ class Services extends React.Component{
             <section className="services">
 				<div className="container">
 					<h2 className="services__heading">Наши услуги на международном рынке</h2>
-					<ul class="grid services__list">
-						<li class="services__item services__item--bigheight services__item--1">
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">A Guide To Rocky Mountain Vacations</a>
+					<ul className="grid services__list">
+						<li className="services__item services__item--bigheight services__item--1">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Жилая недвижимость</a>
+								</h3>
+								<a href="#" className="services__category">Сопровождение сделок по продаже, купли и аренде объектов недвижимости под ключ.</a>
+							</div>
+							<button className="btn-reset services__play">Узнать больше</button>
+						</li>
+						<li className="services__item services__item--2">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Доходная недвижимость</a>
 								</h3>
 							</div>
 						</li>
-						<li class="services__item services__item--2">
-							<button class="btn-reset insp_play" aria-label="Play video"></button>
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">Traveling To Barcelona</a>
+						<li className="services__item services__item--3">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Инвестиционные операции</a>
 								</h3>
 							</div>
 						</li>
-						<li class="services__item services__item--3">
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">Party Jokes Startling But Unnecessary</a>
+						<li className="services__item services__item--big services__item--4">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Покупка яхт</a>
 								</h3>
 							</div>
 						</li>
-						<li class="services__item services__item--big services__item--4">
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">Maui By Air The Best Way Around The Island</a>
+						<li className="services__item services__item--big services__item--5">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Доверительное управление</a>
 								</h3>
 							</div>
 						</li>
-						<li class="services__item services__item--big services__item--5">
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">Man cannot discover new oceans unless he has the courage to lose sight of the shore.</a>
-								</h3>
-							</div>
-						</li>
-						<li class="services__item services__item--6">
-							<button class="btn-reset insp_play" aria-label="Play video"></button>
-							<div class="insp_text">
-								<a href="#" class="insp_category">Destinations</a>
-								<h3 class="insp_title">
-									<a href="#">Traveling To Barcelona</a>
+						<li className="services__item services__item--6">
+							<div className="services__text">
+								<h3 className="services__title">
+									<a href="#">Консьерж сервис</a>
 								</h3>
 							</div>
 						</li>
