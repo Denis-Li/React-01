d6f79a866c79fcf63decf1cf4d50b900f033c563
Denis-Li <den.le.genplan@gmail.com>
2020-11-10 23:27:57 +0100

    work on home 4

--- src/components/assets/MapSvg.js
+++ src/components/assets/MapSvg.js
@@ -7,26 +7,86 @@ import './MapSvg.scss';
 
 function MapSvg() {
     
-        
     const $leftLinks = document.querySelectorAll('.left-menu a'),
         $mapLinks = document.querySelectorAll('.map a'),
         $info = document.querySelectorAll('.info');
 
+    const requestData = (id = 1) => {
+        fetch('data.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            $info.innerHTML = `
+                <h2>${data[id - 1].district}</h2>
+                <p>${data[id - 1].info}</p>
+            `;
+        });
+    };
+
+    requestData();
+    
     $leftLinks.forEach(el => {
         el.addEventListener('mouseenter', (e) => {
-            let self = e.currentTarget;
+            let self = e.currentTarget; //this
             let selfClass = self.getAttribite('href');
             let color = self.dataset.color;
-            let currentElement = document.querySelector(`.map a[href="${selfClass}"]`);
+            let currentElement = document.querySelector(`.map a[href="${selfClass}"]`); //совмещение ссылок
             let currentPolygon = currentElement.querySelectorAll('polygon');
-		    let currentPath = currentElement.querySelectorAll('path');
-		    if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		    if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-		    currentElement.classList.add('active');
+            let currentPath = currentElement.querySelectorAll('path');
+            if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+            if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+            currentElement.classList.add('active');
+        });
+        el.addEventListener('mouseleave', (e) => {
+            let self = e.currentTarget; //this
+            let selfClass = self.getAttribite('href');
+            let currentElement = document.querySelector(`.map a[href="${selfClass}"]`); //совмещение ссылок
+            let currentPolygon = currentElement.querySelectorAll('polygon');
+            let currentPath = currentElement.querySelectorAll('path');
+            if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
+            if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
+            currentElement.classList.remove('active');
         });
     });
 
-
+    $mapLinks.forEach(el => {
+        el.addEventListener('mouseenter', (e) => {
+            let self = e.currentTarget;
+            let selfClass = self.getAttribute('href');
+            let color = self.dataset.color;
+            let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
+            let currentPolygon = self.querySelectorAll('polygon');
+            let currentPath = self.querySelectorAll('path');
+            if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+            if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+            currentElement.classList.add('active');
+        });
+    
+        el.addEventListener('mouseleave', (e) => {
+            let self = e.currentTarget;
+            let selfClass = self.getAttribute('href');
+            let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
+            let currentPolygon = self.querySelectorAll('polygon');
+            let currentPath = self.querySelectorAll('path');
+            if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
+            if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
+            currentElement.classList.remove('active');
+        });
+    
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            let self = e.currentTarget;
+            let selfClass = self.getAttribute('href');
+            let currentElement = document.querySelector(`.left-menu a[href="${selfClass}"]`);
+            let id = parseInt(currentElement.dataset.id);
+            requestData(id);
+        });
+    });
+    
+    
+    
     
     return (
         <>
